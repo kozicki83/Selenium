40e79780bf86e6429e46c53df24daa09e7d153eb
Daniel <d.kozicki83@gmail.com>
2020-08-18 18:48:53 +0200

    Second 01

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -3,9 +3,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
 
-    public static void main(String[] args) {
-
-        System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+    @org.junit.Test
+    public void testA() {
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://www.onet.pl/");
         webDriver.close();
